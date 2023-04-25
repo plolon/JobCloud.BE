2577965f8f0fbc3357fd2e9ea3b299b21ab0a0e4
Dawid Baczak <dawid.banczak@mipcloud.pl>
2023-04-25 21:50:55 +0200

    Added mocked endpoint

---
+++ src/Core/JobCloud.BE.WebApi/Controllers/OfferController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace JobCloud.BE.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfferController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<object>> GetByLanguage(int languageId)
+        {
+            return await GetMockedOffers();
+        }
+
+        // MOCK
+        private async Task<IEnumerable<Offer>> GetMockedOffers()
+        {
+            return new List<Offer>()
+            {
+                new Offer("CumArch", new List<string>{".net", "java", "sql"}, "Intern", new EarningPrice(3000, 5000)),
+                new Offer("NetBubu", new List<string>{"javascript", "css", "html"}, "Mid", new EarningPrice(9000, 13000)),
+                new Offer("someBank", new List<string>{"scala", "sql", "python"}, "Senior", new EarningPrice(21000, 25000)),
+            };
+        }
+
+        private record Offer(string CompanyName, List<string> TechStack, string Level, EarningPrice EarningPrice);
+
+        private record EarningPrice(decimal MinValue, decimal MaxValue);
+    }
+}
